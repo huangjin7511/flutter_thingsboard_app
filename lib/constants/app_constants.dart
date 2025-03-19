830ce4a64d87f51058cbc88573b811950b1de65e
huangjin7511 <54235335+huangjin7511@users.noreply.github.com>
2025-03-19 14:00:54 +0800

    Update app_constants.dart

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,7 +1,7 @@
 abstract class ThingsboardAppConstants {
-  static const thingsBoardApiEndpoint = 'http://localhost:8080';
+  static const thingsBoardApiEndpoint = 'http://tb.xueluo.site';
   static const thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
 
   static const thingsboardIOSAppSecret = 'Your app secret here';
-  static const thingsboardAndroidAppSecret = 'Your app secret here';
+  static const thingsboardAndroidAppSecret = 'd2ZBRHpLYlhRemdJZDhzWk4zSnYwcFB2MjRmMnNndnhvRTMyVXBiS20ycUlWdlB5QUI1d2h2S1EycWJCWEpENg==';
 }
